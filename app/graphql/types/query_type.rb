cf868dc3e6f170b0ff4bb65b7668984a8044e245
y nguyen <y.nguyen@orientsoftware.com>
2022-09-30 15:21:08 +0700

    create post type & user type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,10 +8,26 @@ module Types
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
+    field :test_field, String, null: false, description: "An example field added by the generator"
     def test_field
       "Hello World!"
     end
+
+    # All users:
+    field :users, [Types::UserType], null: false, description: "return all users"
+
+    def users
+      User.all
+    end
+
+    # User:
+    field :user, Types::UserType, null: false, description: "return a user" do
+      argument :id, required: true
+    end
+
+    def users(id:)
+      User.find(id)
+    end
+
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(
+    [
+        {
+            name: "Peter",
+            email: "peter@gmail.com",
+            age: 23,
+            address: "15th Street, Queens, New York City"
+        }, 
+        {
+            name: "Gween",
+            email: "gween@gmail.com",
+            age: 23,
+            address: "SW1E 5BW, Buckingham Gate. Victoria "
+        }, 
+        {
+            name: "Steve",
+            email: "steve@gmail.com",
+            age: 37,
+            address: "141 Worth Street. New York City"
+        }, 
+        {
+            name: "Lizard",
+            email: "lizard@gmail.com",
+            age: 44,
+            address: "Massachusetts Hall Cambridge, MA 02138"
+        }, 
+        {
+            name: "May",
+            email: "may@gmail.com",
+            age: 46,
+            address: "15th Street, Queens, New York City"
+        }, 
+    ]
+)
